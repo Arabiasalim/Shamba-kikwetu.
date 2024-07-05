cffe8f4be332ac7f00d153aa6b157b41a2b354c3
Arabiasalim <arabiasalim123@gmail.com>
2024-07-05 10:45:58 +0300

    Shamba kikwetu final

---
+++ src/SK_frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Hello from "./Routes/home/home.component";
+import Navbar from "./Components/navigation/navigation.component";
+import { Routes, Route } from "react-router-dom";
+import SignInPage from "./Routes/Login/getin.component";
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route path={"/home"} element={<Hello />} />
+                <Route path={"/login"} element={<SignInPage/>}/>
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
